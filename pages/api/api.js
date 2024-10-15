d6f97d12016f6a287522f388d444a089bddfec1d
Enrique Torreblanca <enriquetchat@gmail.com>
2024-10-15 07:29:35 -0700

    Update api.js

--- pages/api/api.js
+++ pages/api/api.js
@@ -1,7 +1,7 @@
 // api/api.js
 import Airtable from 'airtable';
 
-const AIRTABLE_API_KEY = 'patouN6Gr3y651ydi.3643964e3a5852adb6ad1ed36d63ec7ad9019beda6a2f20673ac4bcd021e86af'; // Replace with your actual Airtable API key
+const AIRTABLE_API_KEY = process.env.AIRTABLE_API_KEY; // Replace with your actual Airtable API key
 const AIRTABLE_BASE_ID = 'app6V96kzHKfSguoS'; // Replace with your actual Airtable base ID
 const AIRTABLE_TABLE_NAME = 'Questionnaire Responses'; // Replace with your actual table name
 
